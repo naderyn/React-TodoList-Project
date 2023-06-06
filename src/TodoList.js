9171c97c37096c802ca832d151f83d6ed27c9114
nader <nader_haly@hotmail.com>
2023-06-06 17:58:28 +0300

    Configure React app for deployment to GitHub Pages

---
+++ src/TodoList.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import './styles.css';
+
+class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [],
+      newTodo: '',
+    };
+  }
+
+  componentDidMount() {
+    // Retrieve todos from local storage on component mount
+    const savedTodos = localStorage.getItem('todos');
+    if (savedTodos) {
+      this.setState({ todos: JSON.parse(savedTodos) });
+    }
+  }
+
+  componentDidUpdate() {
+    // Save todos to local storage on state update
+    localStorage.setItem('todos', JSON.stringify(this.state.todos));
+  }
+
+  handleInputChange = (event) => {
+    this.setState({ newTodo: event.target.value });
+  };
+
+  handleAddTodo = () => {
+    const { newTodo, todos } = this.state;
+    if (newTodo.trim() !== '') {
+      const updatedTodos = [...todos, newTodo];
+      this.setState({ todos: updatedTodos, newTodo: '' });
+    }
+  };
+
+  handleDeleteTodo = (index) => {
+    const { todos } = this.state;
+    const updatedTodos = [...todos];
+    updatedTodos.splice(index, 1);
+    this.setState({ todos: updatedTodos });
+  };
+
+  render() {
+    const { todos, newTodo } = this.state;
+
+    return (
+      <div>
+        <h2>To-Do List</h2>
+        <div>
+          <input type="text" value={newTodo} onChange={this.handleInputChange} />
+          <button onClick={this.handleAddTodo}>Add</button>
+        </div>
+        <ul>
+          {todos.map((todo, index) => (
+            <li key={index}>
+              {todo}
+              <button onClick={() => this.handleDeleteTodo(index)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default TodoList;
